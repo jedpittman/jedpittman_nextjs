adf11b9d29027cad9559318bde046708f5a32a1f
Jed Pittman <jedpittman@gmail.com>
2021-10-16 09:51:35 -0400

    fix to invalid site

--- pages/index.tsx
+++ pages/index.tsx
@@ -31,7 +31,7 @@ export default function Home() {
 
           <div className={styles.card}>
             <h2>Projects</h2>
-            <p><a href="https://www.jobhopper.net">Jobhopper: Visualize Job Transitions</a></p>
+            <p><a href="https://www.jobhopper.live">Jobhopper: Visualize Job Transitions</a></p>
             <p><a href="https://github.com/jedpittman">Jed Pittman&apos;s GitHub Profile</a></p>
           </div>
 
